4dac1abdca57e0c3425b274f80cef2dbc7aa77a8
Mark T. Godfrey <mark.godfrey@gatech.edu>
2016-12-21 15:21:00 -0800

    util script for mapping audio urls to singings

---
+++ map_minutes_audio.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import csv
+import util
+
+def map_minutes_audio(conn):
+    curs = conn.cursor()
+
+    reader = csv.reader(open("minutes_audio.csv", 'rb'))
+    reader.next() # skip headers
+    for row in reader:
+        curs.execute("UPDATE minutes SET audio_url=? WHERE Name LIKE ? AND \
+            Date LIKE ?", (row[2], row[0], row[1]))
+
+    conn.commit()
+    curs.close()
+
+if __name__ == '__main__':
+    db = util.open_db()
+    map_minutes_audio(db)
+    db.close()
