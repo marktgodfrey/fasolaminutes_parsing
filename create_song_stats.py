04b2ba9d5b97de9942942964260769840cb3d126
Mike Richards <mrichards42@gmx.com>
2018-01-09 19:56:52 -0500

    Further simplify song stats

--- create_song_stats.py
+++ create_song_stats.py
@@ -4,40 +4,22 @@
 from collections import defaultdict
 import util
 
-def build_counts(conn):
-    # counts[song_id][year] = count
-    counts = {}
-    years = conn.execute("SELECT DISTINCT year FROM minutes").fetchall()
-    for (song_id,) in conn.execute("SELECT id FROM songs"):
-        counts[song_id] = {}
-        for year, in years:
-            counts[song_id][year] = 0
-
-    # Get leads by song and year; compute counts
+def build_ranks(conn):
+    # ranks[year] = sorted [(count, song_id), ...]
+    ranks = defaultdict(list)
     cursor = conn.execute("""
         SELECT song_id, COUNT(*), minutes.Year
         FROM song_leader_joins
         JOIN minutes ON song_leader_joins.minutes_id = minutes.id
         GROUP BY minutes.Year, song_id
+        ORDER BY minutes.Year ASC, COUNT(*) DESC, song_id ASC
     """)
     for (song_id, count, year) in cursor:
-        counts[song_id][year] += count
-
-    return counts
-
-def build_ranks(counts):
-    # ranks[year] = sorted [(count, song_id), ...]
-    ranks = defaultdict(list)
-    for song_id, yearcount in counts.iteritems():
-        for year, count in yearcount.iteritems():
-            ranks[year].append((count, song_id))
-    # Sort desc by count then asc by song_id
-    for data in ranks.itervalues():
-        data.sort(key=lambda i: (-i[0], i[1]))
+        ranks[year].append((count, song_id))
     return ranks
 
 def create_stats(conn):
-    ranks = build_ranks(build_counts(conn))
+    ranks = build_ranks(conn)
     # values = [(song_id, year, lead_count, rank), ...]
     values = []
     for year in sorted(ranks.keys()):
