2a6428fd9fc7a619db42801e3a6a7fdaa5b96591
Mark T. Godfrey <mark.godfrey@gatech.edu>
2020-01-22 12:55:53 -0800

    create new top 20 and location count leader stats

--- create_leader_stats.py
+++ create_leader_stats.py
@@ -29,16 +29,66 @@ def create_counts(conn):
     conn.commit()
     curs.close()
 
+def create_top20_counts(conn):
+    curs = conn.cursor()
+
+    cursor = conn.execute("""
+        SELECT leader_id, COUNT(*)
+        FROM leader_song_stats
+        WHERE lead_rank <= 20 AND lead_count >= 5
+        GROUP BY leader_id
+    """)
+    for (id, count) in cursor:
+        curs.execute("UPDATE leaders SET top20_count=? WHERE id=?", [count, id])
+
+    print("updated top20_counts")
+    conn.commit()
+    curs.close()
+
+def create_location_counts(conn):
+    curs = conn.cursor()
+
+    cursor = conn.execute("""
+        SELECT leader_id, COUNT(DISTINCT(location_id)) as c
+        FROM song_leader_joins
+        INNER JOIN minutes_location_joins ON minutes_location_joins.minutes_id = song_leader_joins.minutes_id
+        WHERE location_id IS NOT NULL
+        GROUP BY leader_id
+        ORDER BY c DESC
+    """)
+    for (id, count) in cursor:
+        curs.execute("UPDATE leaders SET location_count=? WHERE id=?", [count, id])
+
+    print("updated location_counts")
+    conn.commit()
+    curs.close()
+
 def create_stats(conn):
     curs = conn.cursor()
-    curs.execute("""
-        INSERT INTO leader_song_stats (leader_id, song_id, lead_count)
+
+    ranks = defaultdict(list)
+    cursor = conn.execute("""
         SELECT leader_id, song_id, COUNT(*) as count
         FROM song_leader_joins
-        GROUP BY leader_id, song_id
-        ORDER BY leader_id, count DESC
+        GROUP BY song_id, leader_id
+        ORDER BY song_id, count DESC, leader_id
     """)
-    print "created %d leader_song_stats records" % curs.rowcount
+    for (leader_id, song_id, count) in cursor:
+        ranks[song_id].append((count, leader_id))
+
+    values = []
+    for song_id in sorted(ranks.keys()):
+        rank = 1
+        last_count = 0
+        for i, (count, leader_id) in enumerate(ranks[song_id]):
+            if count != last_count:
+                rank = i + 1
+            last_count = count
+            values.append((leader_id, song_id, count, rank))
+
+    conn.executemany("INSERT INTO leader_song_stats (leader_id, song_id, lead_count, lead_rank) VALUES (?, ?, ?, ?)", values)
+
+    print("created %d leader_song_stats records" % len(values))
     conn.commit()
     curs.close()
 
@@ -54,4 +104,6 @@ if __name__ == '__main__':
     delete_stats(db)
     create_stats(db)
     create_counts(db)
+    create_top20_counts(db)
+    create_location_counts(db)
     db.close()
